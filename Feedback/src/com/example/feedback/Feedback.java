2659b96769787977662f5adebc0380e6a0ec06a7
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2015-02-02 22:29:52 +0530

    project with bugs

---
+++ Feedback/src/com/example/feedback/Feedback.java
@@ -0,0 +1,41 @@
+package com.example.feedback;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+public class Feedback extends Activity {
+	
+	
+	
+	private EditText name;
+	private EditText age;
+	private EditText phno;
+	private EditText feedbackbox;
+	private RatingBar ratingbar1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.feedback);
+		
+		
+		name = (EditText) findViewById(R.id.username);
+		
+		age = (EditText) findViewById(R.id.age);
+		phno = (EditText) findViewById(R.id.phno);
+		feedbackbox = (EditText) findViewById(R.id.feedbackbox);
+		ratingbar1 = (RatingBar) findViewById(R.id.ratingbar1);
+		String un= getIntent().getExtras().getString("username");
+		name.setText(un);
+		
+		
+		
+		
+		
+	}
+
+	
+
+}
---
+++ Feedback/src/com/example/feedback/Login.java
@@ -0,0 +1,84 @@
+package com.example.feedback;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends Activity {
+	
+	private Button login;
+	 private EditText username;
+	 private EditText password;
+	 
+	 
+	
+	
+	
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+   
+    
+        OnClickListener listener = new OnClickListener() {
+
+			@Override
+			public void onClick(View x) {
+				
+				if(username.getText().toString()==password.getText().toString())
+				{
+					
+						
+						
+								
+					 Toast.makeText(getApplicationContext(),(CharSequence) password,Toast.LENGTH_LONG).show();
+						
+						Intent feedback = new Intent(getApplicationContext(),Feedback.class);
+						
+						
+						feedback.putExtra("username",username.getText().toString());
+						
+						
+						
+				}
+				
+						
+				
+				
+			
+				
+				else
+				{
+					 
+					Toast.makeText(getApplicationContext(),(CharSequence) password,Toast.LENGTH_LONG).show();
+					
+					
+					
+					Toast.makeText(getApplicationContext(),"Invalid username/password",Toast.LENGTH_LONG).show();
+				}
+			
+			}	
+			
+			};
+
+    
+			login.setOnClickListener(listener);
+    
+    }
+
+
+    
+    
+}
